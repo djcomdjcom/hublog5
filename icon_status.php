259ef2e72de7adc1ed7745ddf84fd158f7e1b1ee
wordpress@djcom.jp <wordpress@djcom.jp>
2021-02-28 16:35:30 +0900

    icon_status.php 修正

--- icon_status.php
+++ icon_status.php
@@ -1,16 +1,21 @@
-<?php
-if ( post_custom( 'event-status' ) == '予約受付中' ):
-  ?>
-<p class="event-status invitation_on">
-予約受付中
-</p>
 
 <?php
-elseif ( post_custom( 'sale-status' ) == '受付終了' ):
+
+
+
+if ( post_custom( 'event-status' ) == 'invitation_on' ):
+  ?>
+<span class="event-status invitation_on w100 maxw-150 mx-auto">
+<img src="<?php bloginfo('stylesheet_directory'); ?>/images/icon_yoyaku.png" alt=""/></span>
+
+<?php
+elseif ( post_custom( 'sale-status' ) == 'invitation_off' ):
   ?>
 <p class="event-status invitation_off">
 受付終了
 </p>
 <?php else:?>
+<span class="event-status invitation_on w100 maxw-180 mx-auto">
+<img src="<?php bloginfo('stylesheet_directory'); ?>/images/icon_invitation_on@2x.png"  alt=""/></span>
 
 <?php endif; ?>
